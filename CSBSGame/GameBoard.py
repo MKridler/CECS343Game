c59516b629a87e3572515d6f9b019a741cc0e774
Jake Schmitz <jake.d.schmitz@gmail.com>
2015-10-15 10:21:48 -0700

    remove some of the getters and setters, getters/setters are a python anti-pattern

--- CSBSGame/GameBoard.py
+++ CSBSGame/GameBoard.py
@@ -51,9 +51,9 @@ class GameBoard(Frame):
         self.w.background = PhotoImage(file = 'img1.gif')
         self.w.create_image(0,0, anchor = NW, image=self.w.background)
 
-        self.token1 = self.w.create_text(825, 1350, fill = 'red', font = 15, text = player1.getName())
-        self.token2 = self.w.create_text(825, 1390, fill = 'red', font = 15, text = player2.getName())
-        self.token3 = self.w.create_text(825, 1430, fill = 'red', font = 15, text = player3.getName())
+        self.token1 = self.w.create_text(825, 1350, fill = 'red', font = 15, text = player1.name)
+        self.token2 = self.w.create_text(825, 1390, fill = 'red', font = 15, text = player2.name)
+        self.token3 = self.w.create_text(825, 1430, fill = 'red', font = 15, text = player3.name)
 
 
         self.vscrollbar.config(command = self.w.yview)
@@ -61,8 +61,8 @@ class GameBoard(Frame):
         self.vscrollbar.pack()
         self.w.pack(expand = YES, side = LEFT, fill = BOTH)
         self.myRooms = Rooms()
-        #print(player1.getLocation())
-        list1 = self.myRooms.roomConnections(player1.getLocation())
+        #print(player1.location)
+        list1 = self.myRooms.roomConnections(player1.location)
 
         for i in list1:
             self.lb1.insert(END, i)
@@ -76,8 +76,8 @@ class GameBoard(Frame):
     def moves(self,player):
 
         self.myRooms = Rooms()
-        print(player.getLocation())
-        list1 = self.myRooms.roomConnections(player.getLocation())
+        print(player.location)
+        list1 = self.myRooms.roomConnections(player.location)
 
         for i in list1:
             self.lb1.insert(END, i)
@@ -86,36 +86,36 @@ class GameBoard(Frame):
     def aiMover(self, player):
 
         self.myRooms = Rooms()
-        print(player.getLocation())
-        list1 = self.myRooms.roomConnections(player.getLocation())
+        print(player.location)
+        list1 = self.myRooms.roomConnections(player.location)
         randloc = random.choice(list1)
-        player.setLocation(randloc)
+        player.location = randloc
         newloc = self.myRooms.roomCoords(randloc)
         x = newloc[0]
         y = newloc[1]
 
-        if player.getName() == 'Izzy':
+        if player.name == 'Izzy':
             self.w.delete(self.token2)
             self.token2 = self.w.create_text(x, y+20, fill = 'red', font = 15, text = 'Izzy')
-            self.text2.insert(END, '\nIzzy has moved to '+ player.getLocation())
+            self.text2.insert(END, '\nIzzy has moved to '+ player.location)
             self.player = self.player3
             self.aiMover(self.player)
-        elif player.getName() == 'Jax':
+        elif player.name == 'Jax':
             newloc = self.myRooms.roomCoords(randloc)
             x = newloc[0]
             y = newloc[1]
             self.w.delete(self.token3)
             self.token3 = self.w.create_text(x, y+40, fill = 'red', font = 15, text = 'Jax')
-            self.text2.insert(END, '\nJax has moved to '+ player.getLocation())
+            self.text2.insert(END, '\nJax has moved to '+ player.location)
             self.player = self.player1
 
     def runClick(self,player):
 
         items =self.lb1.curselection()
         value= self.lb1.get(items[0])
-        print(player.getName())
+        print(player.name)
         #print (items)
-        player.setLocation(value)
+        player.location = value
         newloc = self.myRooms.roomCoords(value)
         x = newloc[0]
         y = newloc[1]
@@ -125,7 +125,7 @@ class GameBoard(Frame):
         #self.token1 = self.w.create_text(x, y, fill = 'red', font = 15, text = 'Donny')
         self.lb1.delete(0, END)
 
-        if player.getName() == 'Donny':
+        if player.name == 'Donny':
             self.w.delete(self.token1)
             self.token1 = self.w.create_text(x, y, fill = 'red', font = 15, text = 'Donny')
             self.moves(self.player)
